7c401e843651233de509a6e29b63c20434192da3
Winchester <113387249+FelixWinchester@users.noreply.github.com>
2023-02-16 22:18:44 +0400

    Update faili-III-6.cpp

--- faili-III-6.cpp
+++ faili-III-6.cpp
@@ -3,13 +3,13 @@
 #include <string>
 using namespace std;
 int main() {
-    ifstream in("sueta.txt");
-    int sunset;
-    while (in >> sunset) {
-        if (sunset < 0) {
-            cout << sunset << "\t";
+    ifstream in("sueta.txt"); //Вхождение в файл.
+    int sunset; //Просто переменная
+    while (in >> sunset) { //Буквально: Читаем до конца файла.
+        if (sunset < 0) { //Проверка на знак числа
+            cout << sunset << "\t"; //Вывод
         }
     }
-    in.close();
+    in.close(); //Выход из файла
     return 0;
 }
