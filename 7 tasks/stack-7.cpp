f009ad01e017f89560b07c29c9b11c1a3e2bae21
Winchester <113387249+FelixWinchester@users.noreply.github.com>
2023-05-04 23:07:23 +0400

    Update stack-7.cpp

--- 7 tasks/stack-7.cpp
+++ 7 tasks/stack-7.cpp
@@ -1,39 +1,95 @@
 #include <iostream>
-#include <stack>
+#include <fstream>
 using namespace std;
 
-int main() {
-
-    int arr[] = {1, 1, 2, 2, 2, 3, 4, 5, 5, 5, 5};
-    int n = sizeof(arr) / sizeof(arr[0]); //размер массива
-    
-    stack<int> st;
-
-    for (int i = 0; i < n; i++) { // Проходим по списку
-        if (st.empty() || arr[i] != st.top()) {  // Если стек пуст или текущий элемент не равен вершине стека, то добавляем элемент в стек
-            st.push(arr[i]);
+// Описание класса Stack
+class Stack {
+// Объявление закрытых полей класса
+private:
+    int* arr; // Указатель на массив элементов стека
+    int top; // Индекс верхнего элемента стека
+    int size; // Размер стека
+// Объявление открытых методов класса
+public:
+    // Конструктор класса Stack
+    Stack(int s) {
+        size = s;
+        arr = new int[size];
+        top = -1;
+    }
+    // Деструктор класса Stack
+    ~Stack() {
+        delete[] arr;
+    }
+    // Метод для добавления элемента в стек
+    void push(int num) {
+        // Проверка на переполнение стека
+        if (top == size - 1) {
+            cout << "Stack overflow" << endl;
+            return;
         }
-        else { // Если текущий элемент равен вершине стека, то пропускаем его
+        top++;
+        arr[top] = num;
+    }
+    // Метод для удаления элемента из стека
+    int pop() {
+        // Проверка на пустоту стека
+        if (top == -1) {
+            cout << "Stack underflow" << endl;
+            return -1;
+        }
+        int num = arr[top];
+        top--;
+        return num;
+    }
+    // Метод для получения верхнего элемента стека без его удаления
+    int peek() {
+        // Проверка на пустоту стека
+        if (top == -1) {
+            cout << "Stack is empty" << endl;
+            return -1;
+        }
+        return arr[top];
+    }
+    // Метод для проверки на пустоту стека
+    bool isEmpty() {
+        return top == -1;
+    }
+};
+
+// Главная функция программы
+int main() {
+    // Открытие файлов для чтения и записи
+    ifstream fin("input.txt");
+    ofstream fout("output.txt");
+    // Чтение размера стека из файла
+    int n;
+    fin >> n;
+    // Создание объекта класса Stack
+    Stack s(n);
+    // Чтение первого элемента из файла и добавление его в стек
+    int num;
+    fin >> num;
+    s.push(num);
+    // Чтение остальных элементов из файла и добавление их в стек с проверкой на повторы
+    for (int i = 1; i < n; i++) {
+        fin >> num;
+        if (num == s.peek()) {
             continue;
         }
-        
-        
-        if (i + 1 < n && arr[i] != arr[i + 1]) {  // Если следующий элемент не равен текущему элементу, то заменяем повторяющиеся элементы на один элемент
-            cout << st.top() << " ";
-            st.pop();
+        else {
+            while (!s.isEmpty()) {
+                fout << s.pop() << " ";
+            }
+            s.push(num);
         }
     }
-    
-    while(!st.empty()) {            // Выводим последний элемент из стека
-        cout << st.top() << " "; // Возвращает ссылку на первый элемент в очереди
-        st.pop();
+    // Вывод оставшихся элементов стека в файл
+    while (!s.isEmpty()) {
+        fout << s.pop() << " ";
     }
-    
+    // Закрытие файлов
+    fin.close();
+    fout.close();
     return 0;
 }
-
-/*  top - вовзращает указатель на вершину стека
-    pop - удаляет элемент из вершины стека
-    empty - возвращает true, если исходный стек пуст, и false в противном случае
-    push - добавляет элемент в вершину стека
-*/
