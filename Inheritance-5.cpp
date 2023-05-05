61de1862c2b1254f3b5dac93af79abc392dc7df7
Winchester <113387249+FelixWinchester@users.noreply.github.com>
2023-05-05 07:38:10 +0400

    Update Inheritance-5.cpp

--- Inheritance-5.cpp
+++ Inheritance-5.cpp
@@ -1,7 +1,7 @@
-#include <iostream> // подключение библиотеки ввода-вывода
-#include <string> // подключение библиотеки для работы со строками
-#include <ctime> // подключение библиотеки для работы со временем
-using namespace std; // использование пространства имен std
+#include <iostream> 
+#include <string> 
+#include <ctime>
+using namespace std;
 
 class Persona { // объявление класса Persona
 protected: // модификатор доступа protected
@@ -35,9 +35,9 @@ public: // модификатор доступа public
 };
 
 class Student : public Enrolle { // объявление класса Student, наследующего класс Enrolle
-protected: // модификатор доступа protected
+protected: 
     int course; // курс
-public: // модификатор доступа public
+public: 
     Student(string s, int d, int m, int y, string f, int c) : Enrolle(s, d, m, y, f), course(c) {} // конструктор класса
     void printInfo() { // метод вывода информации
         cout << "Student: " << surname << ", " << getAge() << " years old, faculty: " << faculty << ", course: " << course << endl; // вывод информации о студенте
@@ -45,17 +45,17 @@ public: // модификатор доступа public
 };
 
 class Teacher : public Enrolle { // объявление класса Teacher, наследующего класс Enrolle
-protected: // модификатор доступа protected
+protected: 
     string position; // должность
     int experience; // опыт работы
-public: // модификатор доступа public
+public: 
     Teacher(string s, int d, int m, int y, string f, string p, int e) : Enrolle(s, d, m, y, f), position(p), experience(e) {} // конструктор класса
     void printInfo() { // метод вывода информации
         cout << "Teacher: " << surname << ", " << getAge() << " years old, faculty: " << faculty << ", position: " << position << ", experience: " << experience << endl; // вывод информации о преподавателе
     }
 };
 
-int main() { // главная функция программы
+int main() { 
     const int n = 5; // количество людей
     Persona* people[n]; // массив указателей на объекты класса Persona
     people[0] = new Enrolle("Ivanov", 10, 2, 2001, "IT"); // создание объекта класса Enrolle
@@ -81,5 +81,4 @@ int main() { // главная функция программы
     for (int i = 0; i < n; i++) { // цикл для удаления объектов
         delete people[i]; // удаление объекта
     }
-    return 0; // завершение программы
-}
+    return 0; 
