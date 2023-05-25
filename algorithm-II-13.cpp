8989decc75f80a49aa68b48bb7b2fb371fad8f88
Winchester <113387249+FelixWinchester@users.noreply.github.com>
2023-05-25 20:10:22 +0400

    Update algorithm-II-13.cpp

--- algorithm-II-13.cpp
+++ algorithm-II-13.cpp
@@ -1,29 +1,41 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include <iostream> 
+#include <vector> 
+#include <algorithm> 
+
 using namespace std;
-class Sound {
+
+class Sound { // объявление класса "Звук"
 public:
-    Sound(int h, int d) : height(h), duration(d) {}
-    int height;
-    int duration;
+    int note; // поле "нота"
+    int duration; // поле "длительность"
+
+    Sound(int n, int d) { // конструктор класса
+        note = n; // инициализация поля "нота"
+        duration = d; // инициализация поля "длительность"
+    }
 };
 
-bool compareDuration(const Sound& a, const Sound& b) {
-    return a.duration < b.duration;
-}
+int main() { // главная функция программы
+    vector<Sound> melody; // создание динамического массива объектов класса "Звук"
+    melody.push_back(Sound(1, 2)); // добавление элемента в массив
+    melody.push_back(Sound(2, 2)); 
+    melody.push_back(Sound(3, 1)); 
+    melody.push_back(Sound(4, 3));
+    melody.push_back(Sound(5, 1)); 
+    melody.push_back(Sound(6, 2)); 
 
-int main() {
-    vector<Sound> melody = { {1, 2}, {2, 3}, {3, 2}, {4, 1}, {5, 2}, {6, 3}, {7, 1} };
-    sort(melody.begin(), melody.end(), compareDuration);
-    vector<Sound> newMelody;
-    int prevDuration = -1;
-    for (auto& sound : melody) {
-        if (sound.duration != prevDuration) {
-            newMelody.push_back(sound);
-            prevDuration = sound.duration;
+    // Удаление звуков одинаковой длительности
+    for (int i = 0; i < melody.size(); i++) { // цикл по элементам массива
+        for (int j = i + 1; j < melody.size(); j++) { // цикл по элементам массива, следующим за текущим
+            if (melody[i].duration == melody[j].duration) { // если длительности элементов равны
+                melody.erase(melody.begin() + j); // удалить элемент из массива
+                j--; // уменьшить счетчик, чтобы не пропустить следующий элемент
+            }
         }
     }
-    melody = newMelody;
-    return 0;
-}
+
+    // Вывод измененной мелодии
+    for (int i = 0; i < melody.size(); i++) { // цикл по элементам массива
+        cout << "Note: " << melody[i].note << " Duration: " << melody[i].duration << endl; // вывод информации о звуке
+    }
+} 
