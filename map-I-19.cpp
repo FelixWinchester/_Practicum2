c889ddeb9c603f85827de88a60f794dcd4ba593e
Winchester <113387249+FelixWinchester@users.noreply.github.com>
2023-06-08 00:44:43 +0400

    Update map-I-19.cpp

--- map-I-19.cpp
+++ map-I-19.cpp
@@ -1,35 +1,75 @@
+// Подключаем необходимые библиотеки
 #include <iostream>
+#include <fstream>
 #include <map>
 #include <vector>
+#include <sstream>
 using namespace std;
 
 int main() {
-    map<int, int> last_occurrence;
-    vector<int> numbers;
-
-    // чтение входных данных
-    int num;
-    while (cin >> num) {
-        numbers.push_back(num);
+    // Открываем входной файл для чтения
+    ifstream inputFile("input.txt");
+    // Проверяем, удалось ли открыть файл
+    if (!inputFile) {
+        cerr << "Unable to open input file" << endl;
+        return 1;
     }
 
-    // обработка входных данных
-    for (int i = 0; i < numbers.size(); i++) {
-        int num = numbers[i];
-        if (last_occurrence.count(num) == 0) {
-            last_occurrence[num] = i;
+    // Открываем выходной файл для записи
+    ofstream outputFile("output.txt");
+    // Проверяем, удалось ли открыть файл
+    if (!outputFile) {
+        cerr << "Unable to open output file" << endl;
+        return 1;
+    }
+
+    // Создаем словарь для хранения последнего вхождения каждого числа
+    map<int, int> last_occurrence;
+    // Создаем вектор для хранения чисел из входного файла
+    vector<int> numbers;
+    // Строка для чтения из входного файла
+    string line;
+    // Переменная для хранения числа, считанного из строки
+    int number;
+
+    // Читаем входной файл построчно
+    while (getline(inputFile, line)) {
+        // Создаем поток для чтения чисел из строки
+        istringstream iss(line);
+        // Считываем числа из потока и добавляем их в вектор
+        while (iss >> number) {
+            numbers.push_back(number);
+            // Запоминаем последнее вхождение числа в словаре
+            last_occurrence[number] = numbers.size() - 1;
         }
     }
+    // Закрываем входной файл
+    inputFile.close();
 
-    // вывод результатов
+    // Обходим вектор чисел и ищем для каждого числа противоположное
     for (int i = 0; i < numbers.size(); i++) {
         int num = numbers[i];
         int opposite_num = -num;
-        if (last_occurrence.count(opposite_num) > 0 && last_occurrence[opposite_num] > last_occurrence[num]) {
-            cout << opposite_num << " ";
+
+        // Если оба числа уже встречались в векторе, выводим информацию о них
+        if (last_occurrence.count(num) && last_occurrence.count(opposite_num)) {
+            outputFile << "Number: " << num << ", Last occurrence: " << last_occurrence[num]
+                       << ", Opposite number: " << opposite_num << ", Last occurrence: " << last_occurrence[opposite_num] << endl;
+        } else if (last_occurrence.count(num)) {
+            // Если только первое число встречалось, выводим информацию о нем и о том, что противоположное не найдено
+            outputFile << "Number: " << num << ", Last occurrence: " << last_occurrence[num]
+                       << ", Opposite number: " << opposite_num << ", Last occurrence: Not found" << endl;
         } else {
-            cout << num << " ";
+            // Если первое число не встречалось, выводим информацию о нем и о последнем вхождении противоположного числа
+            outputFile << "Number: " << num << ", Last occurrence: Not found"
+                       << ", Opposite number: " << opposite_num << ", Last occurrence: " << last_occurrence[opposite_num] << endl;
         }
     }
+
+    // Закрываем выходной файл
+    outputFile.close();
+    // Выводим сообщение о завершении работы программы
+    cout << "Results written to output.txt" << endl;
+    // Возвращаем код успешного завершения программы
     return 0;
 }
