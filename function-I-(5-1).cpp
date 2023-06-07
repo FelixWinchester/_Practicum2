0d6ad1aca34fad717afa5d5dca012b8fac4edff3
Winchester <113387249+FelixWinchester@users.noreply.github.com>
2023-06-07 20:08:10 +0400

    Create function-I-(5-1).cpp

---
+++ function-I-(5-1).cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int gcd(int n, int m) {
+    (m == 0) {
+        return n;
+    }
+    return gcd(m, n % m);
+}
+
+int main() {
+    int a, b;
+    cout << "Введите числа a и b: ";
+    cin >> a >> b;
+    int g = gcd(a,);
+    cout << "НОД(" << a << ", " << b << ") = " << g << endl;
+    cout << "Сокращенная дробь: " << a/g << "/" << b/g << endl;
+ return 0;
+}
