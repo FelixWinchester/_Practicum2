5fce7a034ca5c3814362084dbf6883c01793f56a
Winchester <113387249+FelixWinchester@users.noreply.github.com>
2023-06-08 11:29:21 +0400

    Update set-III-6.cpp

--- set-III-6.cpp
+++ set-III-6.cpp
@@ -8,13 +8,15 @@ using namespace std;
 
 int main() {
     string text = "This is a sample text. It contains multiple sentences. Some of them end with a question mark? Others end with an exclamation mark!";
-
+   set<string> exclamatory_words;
     // Создаем множество для хранения слов в предложениях
     set<string> words;
 
     // Разбиваем текст на предложения
     size_t pos = 0;
+    
     while ((pos = text.find_first_of(".?!", pos)) != string::npos) {
+        
         // Излекаем предложение
         string sentence = text.substr(0, pos + 1);
 
@@ -30,7 +32,8 @@ int main() {
 
             // Добавляем слово в множество
             words.insert(word);
-
+            if(text[pos] == '!')
+               exclamatory_words.insert(word);
             // Переходим к следующему слову
             start = end + 1;
             end = sentence.find(' ', start);
@@ -39,47 +42,18 @@ int main() {
         // Обрабатываем последнее слово в предложении
         string word = sentence.substr(start);
         words.insert(word);
-
+        if(text[pos] == '!')
+               exclamatory_words.insert(word);
         // Переходим к следующему предложению
         text.erase(0, pos + 1);
         pos = 0;
     }
 
     // Создаем множество для хранения слов в восклицательных предложениях
-    set<string> exclamatory_words;
+ 
 
     // Разбиваем текст на восклицательные предложения
-    pos = 0;
-    while ((pos = text.find_first_of("!", pos)) != string::npos) {
-        // Извлекаем предложение
-        string sentence = text.substr(0, pos + 1);
-
-        // Удаляем знаки препинания из предложения
-        sentence.erase(remove_if(sentence.begin(), sentence.end(), ::ispunct), sentence.end());
-
-        // Разбиваем предложение на слова
-        size_t start = 0;
-        size_t end = sentence.find(' ');
-        while (end != string::npos) {
-            // Извлекаем слово
-            string word = sentence.substr(start, end - start);
-
-            // Добавляем слово в множество восклицательных предложений
-            exclamatory_words.insert(word);
-
-            // Переходим к следующему слову
-            start = end + 1;
-            end = sentence.find(' ', start);
-        }
-
-        // Обрабатываем последнее слово в предложении
-        string word = sentence.substr(start);
-        exclamatory_words.insert(word);
-
-        // Переходим к следующему предложению
-        text.erase(0, pos + 1);
-        pos = 0;
-    }
+    
 
     // Создаем можество для хранения слов, которые не встречаются в восклицательных предложениях
     set<string> result;
@@ -91,6 +65,10 @@ int main() {
         cout << word << endl;
     }
     cout << "Total count: " << result.size() << endl;
-
-    return 0;
+    cout << "set1" << endl;
+    for  (const auto& word : exclamatory_words) {
+        cout << word << endl;
+    }
+    return 0; 
+    
 }
